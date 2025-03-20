d9623e70c698af27dd51e13d96a617593d589d03
esteio <101555693+esteio@users.noreply.github.com>
2025-03-20 18:23:15 +0100

    Update verif.ml
    ajout de la version de Théo, en commentaire seulement pour l'instant

--- verif.ml
+++ verif.ml
@@ -28,3 +28,35 @@ et que toutes les déclarations sont correctes.*)
 let verif_prog (p : programme) : bool =
   (* code ici *)
   true
+
+(* version théo
+
+type env_type = { l_variables: (Syntax.idvar * Syntax.typ) list ; l_functions: Syntax.fun_decl list }
+
+let rec check_var v a l = match v,a,l with
+	| _,_,[] -> failwith "use of undeclared variable !"
+	| x,y,(x,y)::l' -> true
+	| x,_,(x,_)::l' -> false
+	| x,y,z::l' -> check_var x y l'
+
+let rec check_fun f a l = match f,a,l with
+	| _,_,[] -> failwith "use of undeclared function !"
+	| x,y,(x,_,y,_)::l' -> true
+	| x,_,(x,_,_,_)::l' -> false
+	| x,y,z::l' check_fun x y l'
+
+let rec verif_expr expression type_attendu environment = match (expression,type_attendu,environment) with
+	| (Int i,TInt,_) -> true
+	| (Bool b,TBool,_) -> true
+	| (Var v,attente,env) -> check_var v attente env.l_variables
+	| (IdFun f,attente,env) -> check_fun f attente env.l_functions
+	|
+	| _ -> false
+
+let verif_decl_fun funct environment = match (funct,environment) with
+	| _ -> false
+
+let verif_prog program = match program with
+	| _ -> false
+ 
+*)
