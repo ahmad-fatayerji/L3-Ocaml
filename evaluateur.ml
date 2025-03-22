13cd0c5ce0e1809091f1ea24bc080384bd7805d8
esteio <101555693+esteio@users.noreply.github.com>
2025-03-22 20:52:20 +0100

    Update evaluateur.ml

--- evaluateur.ml
+++ evaluateur.ml
@@ -8,10 +8,10 @@ open Syntax
 
 type t_val = V_int of int | V_bool of bool
 
+type env_val = { l_variables: (idvar * t_val) list ; l_functions: (fun_decl * t_val) list }
+
 let eval_expr expression environment = match (expression,environment) with
 	| _ -> ()
 
 let eval_prog program = match program with
 	| _ -> ()
-
-type env_val = { l_variables: (idvar * t_val) list ; l_functions: (fun_decl * t_val) list }
